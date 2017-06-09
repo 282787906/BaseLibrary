e6ddc5291d247c5f30fa275bb9d734f09d24131b
Liqg <282787906@qq.com>
2017-06-09 09:16:34 +0800

    zbar library封装

--- app/src/main/java/com/lee/base/activity/ScanActivity.java
+++ app/src/main/java/com/lee/base/activity/ScanActivity.java
@@ -1,40 +1,61 @@
 package com.lee.base.activity;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
 
 import com.lee.base.R;
 import com.lee.base.core.utils.ToastUtil;
+import com.lee.base.qrcode.zbar.OnScanListener;
+import com.lee.base.qrcode.zbar.ScanView;
 
-import me.dm7.barcodescanner.zbar.Result;
-import me.dm7.barcodescanner.zbar.ZBarScannerView;
+public class ScanActivity extends BaseActivity   {
 
-public class ScanActivity extends BaseActivity implements ZBarScannerView.ResultHandler {
-
-    ZBarScannerView zbar_View;
+    ScanView scanView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-        zbar_View = (ZBarScannerView) findViewById(R.id.activity_scan_zbar_View);
+        scanView = (ScanView) findViewById(R.id.activity_scan_zbar_View);
+        scanView.init(new OnScanListener() {
+            @Override
+            public void onScanSuccess(String result) {
+                ToastUtil.longToast(mContext,result);
+            }
+
+            @Override
+            public void onScanError(Exception ex, String result) {
+                ToastUtil.longToast(mContext,"onScanError"+result);
+ex.printStackTrace();
+            }
+
+            @Override
+            public void onScanStart() {
+                ToastUtil.longToast(mContext,"onScanStart");
+
+            }
+        });
+        scanView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.longToast(mContext,"reScan");
+                scanView.reScan();
+
+            }
+        });
     }
 
-    @Override
-    public void handleResult(Result result) {
-        ToastUtil.showSingleToast(this, result.getContents(), Toast.LENGTH_LONG);
 
-    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        zbar_View.startCamera();
+        scanView.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        zbar_View.stopCamera();
+        scanView.onPause();
     }
 }
